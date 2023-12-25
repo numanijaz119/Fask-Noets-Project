5bc28b47948d5ae7aee9e01b1392c7c9365ca286
numanijaz119 <minibhai009@gmail.com>
2023-12-25 15:26:34 +0500

    user login and showing webpages basd on that.

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
         flash('Incorrect Password, try again.', category='error')
     else:
       flash('Email does not exist', category='error')
-  return render_template("login.html")
+  return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
@@ -58,7 +58,7 @@ def sign_up():
       new_user = User(email=email, first_name= first_name, password=hashed_password)
       db.session.add(new_user)
       db.session.commit()
-      login_user(user, remember=True)
+      login_user(new_user, remember=True)
       flash('Account created!', category='success')
       return redirect(url_for('views.home'))
-  return render_template("sign_up.html")
+  return render_template("sign_up.html", user=current_user)
