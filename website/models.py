0ef4c9582f04900d5a14cc57f3daedbf185e656c
numanijaz119 <minibhai009@gmail.com>
2023-12-27 14:30:44 +0500

    new database and you can add notes.

--- website/models.py
+++ website/models.py
@@ -1,11 +1,12 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from datetime import datetime
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    note_data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime, default=func.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
